8c1e0786b87fb994c242cef21e7d1311da0e7f4f
KiVoPhong <thanthoai999999999@gmail.com>
2023-01-07 13:23:58 +0700

    t

--- app/src/main/java/com/kivophong/simplequizapp/MainActivity.java
+++ app/src/main/java/com/kivophong/simplequizapp/MainActivity.java
@@ -1,14 +1,92 @@
 package com.kivophong.simplequizapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] PERMISSIONS = {"Manifest.permission.WRITE_EXTERNAL_STORAGE"};
+    private ActivityResultLauncher<String[]> requestPermissionLauncher;
+    private ActivityResultContracts.RequestMultiplePermissions requestMultiplePermissions;
+    ListView superListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestMultiplePermissions = new ActivityResultContracts.RequestMultiplePermissions();
+        requestPermissionLauncher = registerForActivityResult(requestMultiplePermissions, isGranted -> {
+            Log.d("PERMISSIONS", "Launcher result: " + isGranted.toString());
+            if (isGranted.containsValue(false)) {
+                Log.d("PERMISSIONS", "At least one of the permissions was not granted, launching again...");
+                requestPermissionLauncher.launch(PERMISSIONS);
+            }
+        });
+
+        askPermissions(requestPermissionLauncher);
+        superListView = findViewById(R.id.superListView);
+        getSuperHeroes();
+   }
+
+    private void askPermissions(ActivityResultLauncher<String[]> multiplePermissionLauncher) {
+        if (!hasPermissions(PERMISSIONS)) {
+            Log.d("PERMISSIONS", "Launching multiple contract permission launcher for ALL required permissions");
+            multiplePermissionLauncher.launch(PERMISSIONS);
+        } else {
+            Log.d("PERMISSIONS", "All permissions are already granted");
+        }
     }
+
+    private boolean hasPermissions(String[] permissions) {
+        if (permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PERMISSIONS", "Permission is not granted: " + permission);
+                    return false;
+                }
+                Log.d("PERMISSIONS", "Permission already granted: " + permission);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void getSuperHeroes() {
+        Call<List<Results>> call = RetrofitClient.getInstance().getMyApi().getsuperHeroes();
+        call.enqueue(new Callback<List<Results>>() {
+            @Override
+            public void onResponse(Call<List<Results>> call, Response<List<Results>> response) {
+                List<Results> myheroList = response.body();
+                String[] oneHeroes = new String[myheroList.size()];
+
+                for (int i = 0; i < myheroList.size(); i++) {
+                    oneHeroes[i] = myheroList.get(i).getName();
+                }
+
+                superListView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, oneHeroes));
+            }
+
+            @Override
+            public void onFailure(Call<List<Results>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "An error has occured", Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+
 }
